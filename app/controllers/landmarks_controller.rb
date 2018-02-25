28ec63985ae69109e9f2a08fd563f15b1b76ef76
Rauof M <abdelrauof197@gmail.com>
2018-02-25 05:07:49 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,22 +1,22 @@
 class LandmarksController < ApplicationController
 
-  get '/figures' do 
-    erb :'/figures/index'
+  get '/landmarks' do 
+    erb :'/landmarks/index'
   end
   
   get '/figures/new' do 
-    erb :'/figures/new'
+    erb :'/landmarks/new'
   end 
   
-  post '/figures' do 
+  post '/landmarks' do 
     @figure = Figure.create(params[:figure])
     @figure.landmarks << Landmark.create(params[:landmark][:name]) unless params[:landmark][:name].empty?
     @figure.titles << Title.create(params[:title][:name]) unless params[:title][:name].empty?
     @figure.save
-    erb :'/figures/show'
+    erb :'/landmarks/show'
   end
   
-  get '/figures/:id' do 
+  get '/landmarks/:id' do 
     @figures = Figure.find(params[:id])
     redirect :"/figures/#{@figure.id}"
   end
@@ -26,12 +26,12 @@ class LandmarksController < ApplicationController
     erb :"/figures/edit"
   end
   
-  patch '/figures/:id' do 
+  patch '/landmarks/:id' do 
     @figure = Figure.update(params[:figure])
     @figure.landmarks << Landmark.create(params[:landmark][:name]) unless params[:landmark][:name].empty?
     @figure.titles << Title.create(params[:title][:name]) unless params[:title][:name].empty?
     @figure.save
-    redirect :"/figures/#{@figure.id}"
+    redirect :"/landmarks/#{@figure.id}"
   end
 
 
